2adc7c12d340cd8b7ddd0fccfa594283d2959db3
EyeEnyx <61381475+EyeEnyx@users.noreply.github.com>
2021-04-02 20:28:43 +0800

    Update application-questions.js

--- application-questions.js
+++ application-questions.js
@@ -1,5 +1,11 @@
 module.exports =  [
-	"question 1",
-	"question 2",
-	"question 3"
+	"What is your "In Game Name#tagline"? (Valorant or other games)",
+	"How old are you? (Your privacy is protected)",
+	"How did you found the Dynasty discord?",
+	"What kind of gamer do you classify yourself? (you can put many letters)A. Looking for teammates\nB. Competitive\nC. Streamer\nD. Others (please tell me more about you)",
+	"Are you willing to make Dynasty Organization discord as your main discord server?",
+	" Will you pledge to FOLLOW the rules and policies of "Dynasty Organization"? you can check at #:o:-dynasty-rules",
+	"What is your facebook link? (If a dummy account, you'll be requested a real-time picture or webcam stream on your interview)",
+	"[Required] Join this member-only facebook group. (https://www.facebook.com/groups/dynastygamingcommunity) -Type 'done' if you joined.",
+	"Kindly, wait for your scheduled interview. Our Moderators will send you a message. Thank you for your time. Type 'done'."
 ];
